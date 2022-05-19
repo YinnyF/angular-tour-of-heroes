2dbc4181a8156153c1a14427284fe4b70b7d19f2
cynthia-switchd <yinnycfu@gmail.com>
2022-05-19 12:38:56 +0100

    feat: update a hero with name persist

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -15,6 +15,10 @@ export class HeroService {
 
   private heroesUrl = 'api/heroes'; // URL to web api
 
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  }
+
   constructor(
     private http: HttpClient,
     private messageService: MessageService
@@ -35,6 +39,14 @@ export class HeroService {
       .pipe(
         tap(_ => this.log(`fetched hero id=${id}`)),
         catchError(this.handleError<Hero>(`getHero id=${id}`))
+      );
+  }
+
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions)
+      .pipe(
+        tap(_ => this.log(`update hero id=${hero.id}`)),
+        catchError(this.handleError<any>('updateHero'))
       );
   }
 
