6c10482ba6a1c71774b75db497a432f162a832e5
AzureAD\CynthiaFu <cynthia.fu@paymentsense.com>
2022-06-21 22:13:35 +0100

    test: add unit tests for message service

--- src/app/message.service.spec.ts
+++ src/app/message.service.spec.ts
@@ -13,4 +13,22 @@ describe('MessageService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should have no messages to start', () => {
+    expect(service.messages.length).toBe(0);
+  })
+
+  it('should add a message when add is called', () => {
+    service.add('message 1');
+
+    expect(service.messages.length).toBe(1);
+  })
+
+  it('should clear messages when clear is called', () => {
+    service.add('message 1');
+    
+    service.clear();
+    
+    expect(service.messages.length).toBe(0);
+  })
 });
