5209f40bc96c155a1e545bddd119dfc95e7a391f
AzureAD\CynthiaFu <cynthia.fu@paymentsense.com>
2022-06-22 15:14:07 +0100

    test: hero service - create spies for service dependencies

--- src/app/services/hero.service.spec.ts
+++ src/app/services/hero.service.spec.ts
@@ -1,17 +1,38 @@
+import { HttpClient } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 
 import { HeroService } from './hero.service';
+import { MessageService } from './message.service';
 
 describe('HeroService', () => {
-  let service: HeroService;
+  let heroService: HeroService;
+  let messageServiceSpy: jasmine.SpyObj<MessageService>;
+  let httpClientSpy: jasmine.SpyObj<HttpClient>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(HeroService);
+    const spy = jasmine.createSpyObj('MessageService', [
+      'add'
+    ])
+
+    const clientSpy = jasmine.createSpyObj('HttpClient', [
+      'get'
+    ]);
+
+    TestBed.configureTestingModule({
+      providers: [
+        HeroService,
+        { provide: MessageService, useValue: spy },
+        { provide: HttpClient, useValue: clientSpy }
+      ]
+    });
+
+    heroService = TestBed.inject(HeroService);
+    messageServiceSpy = TestBed.inject(MessageService) as jasmine.SpyObj<MessageService>;
+    httpClientSpy = TestBed.inject(HttpClient) as jasmine.SpyObj<HttpClient>;
   });
 
-  xit('should be created', () => {
-    expect(service).toBeTruthy();
+  it('should be created', () => {
+    expect(heroService).toBeTruthy();
   });
 
   xit('should return a hero')
